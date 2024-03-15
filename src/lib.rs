6ad0bd62290f143ea85d1d8eb1d09e0ff2039dfe
Lucas Baschinski <12460927+lbaschinski@users.noreply.github.com>
2024-03-15 11:42:23 +0100

    Adds user input for timer length

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+use std::io;
+
 mod cards;
 mod dice;
 mod timer;
 
 pub fn start_game() {
+    println!("Please write down the current timeout:");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let timeout = timer::TimeoutFromString::new(input);
+
     let letter = dice::roll_dice();
     println!("Your chosen letter is: {letter}");
     let card = cards::draw_card();
@@ -10,6 +19,6 @@ pub fn start_game() {
     for category in card {
         println!("- {category}")
     }
-    timer::start_timer(60);
+    timer::start_timer(timeout);
     println!("Your time is over!")
 }
--- src/timer.rs
+++ src/timer.rs
@@ -2,12 +2,35 @@ use std::{thread, time::Duration};
 use std::io::{Write, stdout};
 use crossterm::{QueueableCommand, cursor, terminal, ExecutableCommand};
 
-pub fn start_timer(timeout: u32) {
-    if timeout > 999 {
-        panic!("Please provide a timeout lower than 999s!");
+// Timeout struct for input validation
+pub struct TimeoutFromString {
+    // this is private, so that `new` must be used and value is validated
+    value: u32,
+}
+impl TimeoutFromString {
+    pub fn new(input: String) -> TimeoutFromString {
+        let value: u32 = match input.trim().parse() {
+            Ok(value) => {
+                if value < 1 || value > 999 {
+                    panic!("Please provide a positive timeout lower than 999s!");
+                }
+                value
+            },
+            Err(_) => {
+                panic!("Please type a number!");
+            }
+        };
+        TimeoutFromString { value }
     }
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
+pub fn start_timer(timeout_from_string: TimeoutFromString) {
     println!("Timer:");
     let mut stdout = stdout();
+    let timeout = timeout_from_string.value();
 
     stdout.execute(cursor::Hide).unwrap();
     for s in 0..(timeout+1) {
