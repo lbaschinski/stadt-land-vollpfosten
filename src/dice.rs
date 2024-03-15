e01f495f32de31887dfe004f8857c2ff0f32e43d
Lucas Baschinski <12460927+lbaschinski@users.noreply.github.com>
2024-03-15 13:58:33 +0100

    Adds random dice throw functionality

--- src/dice.rs
+++ src/dice.rs
@@ -1,3 +1,42 @@
+use rand::seq::SliceRandom;
+
+/// Special 30 sided dice from the game.
+/// Is missing X and Y, therefore A, E, O and S are doubled.
+/// Has two wildcard (⚡) sides as well.
+const DICE: [&str; 30] =
+    [ "A"
+    , "A" // no error, is twice on the dice
+    , "B"
+    , "C"
+    , "D"
+    , "E"
+    , "E" // no error, is twice on the dice
+    , "F"
+    , "G"
+    , "H"
+    , "I"
+    , "J"
+    , "K"
+    , "L"
+    , "M"
+    , "N"
+    , "O"
+    , "O" // no error, is twice on the dice
+    , "P"
+    , "Q"
+    , "R"
+    , "S"
+    , "S" // no error, is twice on the dice
+    , "T"
+    , "U"
+    , "V"
+    , "W"
+    , "Z"
+    , "⚡" // choose your own letter
+    , "⚡" // no error, is twice on the dice
+    ];
+
 pub fn roll_dice() -> &'static str {
-    "A"
+    let mut rng = rand::thread_rng();
+    DICE.choose(&mut rng).unwrap()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 use std::io;
+use std::io::prelude::*;
 
 mod cards;
 mod dice;
 mod timer;
+
+/// Wait for user input to continue
+fn wait_for_user() {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line,
+    // so we print without a newline and flush manually.
+    write!(stdout, "Press Enter (↵) to continue...").unwrap();
+    stdout.flush().unwrap();
+
+    // Read everything and discard
+    let _ = stdin.read_line(&mut String::new()).unwrap();
+}
 
 pub fn start_game() {
     println!("Please write down the current timeout:");
@@ -13,7 +28,9 @@ pub fn start_game() {
     let timeout = timer::TimeoutFromString::new(input);
 
     let letter = dice::roll_dice();
-    println!("Your chosen letter is: {letter}");
+    println!("Your letter is: {letter}");
+    wait_for_user();
+
     let card = cards::draw_card();
     println!("Your card contains the following categories:");
     for category in card {
