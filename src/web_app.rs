1bbf7fa43eed128f26529e9464b8082b50dc01cd
Lucas Baschinski <12460927+lbaschinski@users.noreply.github.com>
2024-03-25 09:54:26 +0100

    Improve types and app flow

--- src/web_app.rs
+++ src/web_app.rs
@@ -10,44 +10,39 @@ use minijinja::{context, Environment};
 use serde::Deserialize;
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::ops::Deref;
 use crate::cards;
 use crate::dice;
 
-struct AppState {
+struct GameState {
     categories: Mutex<Vec<String>>,
-    timeout: Mutex<u32>,
-    letter: Mutex<Option<char>>,
-    card: Mutex<Option<Vec<String>>>,
+    round_state: Mutex<RoundState>,
     environment: Environment<'static>,
 }
 
-#[derive(Deserialize, Debug)]
-#[allow(dead_code)]
-struct PrepareInput {
-    category: String,
+struct RoundState {
+    timeout: u32,
+    letter: Option<char>,
+    card: Option<Vec<(String, bool)>>,
+}
+
+impl RoundState {
+    pub fn new(timeout: u32, letter: Option<char>, card: Option<Vec<(String, bool)>>) -> RoundState {
+        RoundState { timeout, letter, card }
+    }
 }
 
 #[derive(Deserialize, Debug)]
 #[allow(dead_code)]
 struct GameInput {
-    timeout: u32,
-    letter: Option<char>,
+    category: String,
 }
 
 #[derive(Deserialize, Debug)]
 #[allow(dead_code)]
 struct RoundInput {
     timeout: u32,
-    letter: char,
+    letter: Option<char>,
     current: Option<usize>,
-}
-
-#[derive(Deserialize, Debug)]
-#[allow(dead_code)]
-struct ResultInput {
-    timeout: u32,
-    letter: char,
 }
 
 pub async fn serve() {
@@ -55,34 +50,32 @@ pub async fn serve() {
     env.add_template("layout", include_str!("./templates/layout.jinja")).unwrap();
     env.add_template("home", include_str!("./templates/home.jinja")).unwrap();
     env.add_template("categories", include_str!("./templates/categories.jinja")).unwrap();
-    env.add_template("prepare", include_str!("./templates/prepare.jinja")).unwrap();
     env.add_template("start", include_str!("./templates/start.jinja")).unwrap();
     env.add_template("round", include_str!("./templates/round.jinja")).unwrap();
+    env.add_template("timer", include_str!("./templates/timer.jinja")).unwrap();
     env.add_template("result", include_str!("./templates/result.jinja")).unwrap();
 
-    let app_state = Arc::new(AppState
+    let game_state = Arc::new(GameState
         { categories: Mutex::new(Vec::new())
-        , timeout: Mutex::new(0)
-        , letter: Mutex::new(None)
-        , card: Mutex::new(None)
+        , round_state: Mutex::new(RoundState::new(0, None, None))
         , environment: env
         });
 
     let app = Router::new()
         .route("/", get(handler_home))
-        .route("/prepare", get(handler_prepare_game).post(post_prepare_game))
-        .route("/categories", get(handler_categories))
         .route("/start", get(handler_start_game).post(post_start_game))
-        .route("/round", post(post_start_round))
+        .route("/categories", get(handler_categories))
+        .route("/round", get(handler_start_round).post(post_start_round))
+        .route("/timer", post(post_start_timer))
         .route("/result", post(post_result))
-        .with_state(app_state);
+        .with_state(game_state);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3333").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn handler_home(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
+async fn handler_home(State(state): State<Arc<GameState>>) -> Result<Html<String>, StatusCode> {
     let template = state.environment.get_template("home").unwrap();
 
     let rendered = template
@@ -95,8 +88,8 @@ async fn handler_home(State(state): State<Arc<AppState>>) -> Result<Html<String>
     Ok(Html(rendered))
 }
 
-async fn handler_prepare_game(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
-    let template = state.environment.get_template("prepare").unwrap();
+async fn handler_start_game(State(state): State<Arc<GameState>>) -> Result<Html<String>, StatusCode> {
+    let template = state.environment.get_template("start").unwrap();
 
     let rendered = template
         .render(context! {
@@ -107,7 +100,7 @@ async fn handler_prepare_game(State(state): State<Arc<AppState>>) -> Result<Html
     Ok(Html(rendered))
 }
 
-async fn post_prepare_game(State(state): State<Arc<AppState>>, Form(input): Form<PrepareInput>) -> Result<Html<String>, StatusCode> {
+async fn post_start_game(State(state): State<Arc<GameState>>, Form(input): Form<GameInput>) -> Result<Html<String>, StatusCode> {
     let template = state.environment.get_template("categories").unwrap();
     let categories = cards::load_categories(&input.category);
     {
@@ -125,7 +118,7 @@ async fn post_prepare_game(State(state): State<Arc<AppState>>, Form(input): Form
     Ok(Html(rendered))
 }
 
-async fn handler_categories(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
+async fn handler_categories(State(state): State<Arc<GameState>>) -> Result<Html<String>, StatusCode> {
     let template = state.environment.get_template("categories").unwrap();
 
     let rendered = template
@@ -138,94 +131,93 @@ async fn handler_categories(State(state): State<Arc<AppState>>) -> Result<Html<S
     Ok(Html(rendered))
 }
 
-async fn handler_start_game(State(state): State<Arc<AppState>>) -> Result<Html<String>, StatusCode> {
-    let template = state.environment.get_template("start").unwrap();
+async fn handler_start_round(State(state): State<Arc<GameState>>) -> Result<Html<String>, StatusCode> {
+    let template = state.environment.get_template("round").unwrap();
 
     let rendered = template
         .render(context! {
-            title => "Start Game",
-            timeout => state.timeout,
-            letter => state.letter,
+            title => "Start Round",
         })
         .unwrap();
 
     Ok(Html(rendered))
 }
 
-async fn post_start_game(State(state): State<Arc<AppState>>, Form(input): Form<GameInput>) -> Result<Html<String>, StatusCode> {
-    let template = state.environment.get_template("start").unwrap();
-    {
-        let mut timeout = state.timeout.lock().unwrap();
-        *timeout = input.timeout;
-        let mut letter = state.letter.lock().unwrap();
-        *letter = input.letter;
-    }
+async fn post_start_round(State(state): State<Arc<GameState>>, Form(input): Form<RoundInput>) -> Result<Html<String>, StatusCode> {
+    let template = state.environment.get_template("round").unwrap();
+
+    let mut round_state = state.round_state.lock().unwrap();
+    *round_state = RoundState::new(input.timeout, input.letter, round_state.card.clone());
 
     if input.letter.is_some() {
-        let mut letter = state.letter.lock().unwrap();
-        *letter = dice::roll_dice().chars().next();
-    }
-
-    let rendered = template
-        .render(context! {
-            title => "Start Game",
-            timeout => state.timeout,
-            letter => state.letter,
-        })
-        .unwrap();
-
-    Ok(Html(rendered))
-}
-
-async fn post_start_round(State(state): State<Arc<AppState>>, Form(input): Form<RoundInput>) -> Result<Html<String>, StatusCode> {
-    let template = state.environment.get_template("round").unwrap();
-    let category: String;
-    let next: usize;
-    let drawn_card: Vec<String>;
-    {
-        let mut timeout = state.timeout.lock().unwrap();
-        *timeout = input.timeout;
-        let mut letter = state.letter.lock().unwrap();
-        *letter = Some(input.letter);
-        // TODO: handle empty list when prepare was not done
-        let categories = state.categories.lock().unwrap();
-        let mut card = state.card.lock().unwrap();
-        drawn_card = match card.deref() {
-            Some(c) => c.to_vec(),
-            None => cards::draw_card(&categories, 6),
-        };
-        *card = Some(drawn_card.clone());
-        let current_num = match input.current {
-            Some(num) => num,
-            None => 0,
-        };
-        next = if current_num == 5 { 0 } else { current_num + 1 };
-        category = drawn_card[current_num].clone();
+        let letter = dice::roll_dice().chars().next();
+        *round_state = RoundState::new(round_state.timeout, letter, round_state.card.clone());
     }
 
     let rendered = template
         .render(context! {
             title => "Start Round",
-            timeout => state.timeout,
-            letter => state.letter,
-            category => category,
-            next => next,
-            card => state.card,
+            timeout => round_state.timeout,
+            letter => round_state.letter,
         })
         .unwrap();
 
     Ok(Html(rendered))
 }
 
-async fn post_result(State(state): State<Arc<AppState>>, Form(input): Form<ResultInput>) -> Result<Html<String>, StatusCode> {
+async fn post_start_timer(State(state): State<Arc<GameState>>, Form(input): Form<RoundInput>) -> Result<Html<String>, StatusCode> {
+    let template = state.environment.get_template("timer").unwrap();
+    let category: String;
+    let next: usize;
+    let drawn_card: Vec<(String, bool)>;
+
+    let mut round_state = state.round_state.lock().unwrap();
+    // TODO: handle empty list when prepare was not done
+    let categories = state.categories.lock().unwrap();
+    drawn_card = match &round_state.card {
+        Some(c) => c.to_vec(),
+        None => {
+            let dc = cards::draw_card(&categories, 6);
+            let mut dc_tuple = Vec::new();
+            for c in dc {
+                dc_tuple.push((c, false));
+            }
+            dc_tuple
+        },
+    };
+    *round_state = RoundState::new(input.timeout, input.letter, Some(drawn_card.clone()));
+    let current_num = match input.current {
+        Some(num) => num,
+        None => 0,
+    };
+    next = if current_num == 5 { 0 } else { current_num + 1 };
+    category = drawn_card[current_num].0.clone();
+
+    let rendered = template
+        .render(context! {
+            title => "Start Timer",
+            timeout => round_state.timeout,
+            letter => round_state.letter,
+            category => category,
+            next => next,
+            card => round_state.card,
+        })
+        .unwrap();
+
+    Ok(Html(rendered))
+}
+
+async fn post_result(State(state): State<Arc<GameState>>, Form(input): Form<RoundInput>) -> Result<Html<String>, StatusCode> {
     let template = state.environment.get_template("result").unwrap();
+
+    let round_state = state.round_state.lock().unwrap();
 
     let rendered = template
         .render(context! {
             title => "Round Results",
             timeout => input.timeout,
             letter => input.letter,
-            card => state.card,
+            card => round_state.card,
         })
         .unwrap();
 
